a6e26f88e409bf9ff1595cd2082f7418ba5b4984
Hawkins-Stark <hawk5052@vandals.uidaho.edu>
2024-02-12 12:00:36 -0800

    Working on creating custom config for modbus server

--- Extras/pyModbusScript.py
+++ Extras/pyModbusScript.py
@@ -1,6 +1,8 @@
 import asyncio
-
+import json
 from pymodbus.server import ModbusSimulatorServer
+
+# PyModBus Documentation: https://pymodbus.readthedocs.io/en/latest/source/library/simulator/config.html
 
 async def run():
    simulator = ModbusSimulatorServer(
@@ -8,12 +10,31 @@ async def run():
       modbus_device = "my device",
       http_host = "localhost",
       http_port =  8081,
-      json_file = "customSetup.json")
+      json_file = "custom_single_device.json")
 
    await simulator.run_forever()
 
-while(True):
-   # print("I'm Running I swear....")
-   # run()
-   # await simulator.stop()
-   asyncio.run(run())
+def read_json_config_file():
+    with open('custom_single_device.json', 'r') as f:
+      data = json.load(f)
+    return data
+ 
+def create_custom_config_file():
+   # Some documentation notes:
+   # Bits: Registers can be singulars (first entry) or arrays (second entry)
+   # Uint16: Registers can be singulars (first entry) or arrays (second entry)
+   # Uint32: Registers can only be arrays in multiples of 2
+   # Float32: Registers can only be arrays in multiples of 2
+   # Strings: Register 1 is a string of 2 chars, Register 2 is a string of 4 chars, Register 3 is a string of 10 chars with the value ‘’A_B_C_D_E_’’.
+   pass
+
+def run_web_server():
+    while(True):
+       # print("I'm Running I swear....")
+       # run()
+       # await simulator.stop()
+       asyncio.run(run())
+   
+if __name__ == "__main__":
+    default_config = read_json_config_file()
+    print(default_config)
