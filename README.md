b382196e557a83c35a62da4ee55f435daac73e3f
Hawkins-Stark <hawk5052@vandals.uidaho.edu>
2024-03-08 13:12:33 -0800

    Trying with new git ignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# aph_unreal_model
--- TestProject01/Intermediate/Build/Win64/x64/TestProject01Editor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/Users/hawk5052/Documents/Unreal Projects/aph_unreal_model/TestProject01/Intermediate/Build/Win64/x64/TestProject01Editor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
