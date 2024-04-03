1e5e29471567f04471e39351f48f698112394f9f
hunterdhawkins <hunterdhawkins@gmail.com>
2024-04-03 12:21:05 -0700

    Can now save multiple replay templates

--- Extras/webserver/home/forms.py
+++ Extras/webserver/home/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class ReplayForm(forms.Form):
     replay_name = forms.CharField(label="Replay Name", max_length=256)
-    starting_timestamp = forms.DateTime()
-    ending_timestamp = forms.DateTime()
+    starting_timestamp = forms.DateTimeField()
+    ending_timestamp = forms.DateTimeField()
 
--- Extras/webserver/home/views.py
+++ Extras/webserver/home/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse, HttpResponse, StreamingHttpResponse
 from django.conf import settings
 from home import utils, models
+from home.forms import ReplayForm
 
 
 def home(request):
@@ -30,6 +31,8 @@ def display_single_data_point_graph(request):
 def control_method(request):
 	control_mode="real-time"
 
+	all_replay_templates = models.Replay.objects.all()
+
 	if request.method == 'POST':
 		control_mode = request.POST.get("control_mode")
 		print(control_mode)
@@ -39,15 +42,45 @@ def control_method(request):
 					"home/control_method.html",
 					{
 						'current_control_mode': control_mode,
+						'all_replay_templates': all_replay_templates,
+
 					}
 	)
 
 
 def create_replay_template(request):
-    
+
+	if request.method == "POST":
+		# create a form instance and populate it with data from the request:
+		form = ReplayForm(request.POST)
+		print(form.errors)
+		# check whether it's valid:
+		if form.is_valid():
+			rn = form.cleaned_data['replay_name']
+			st = form.cleaned_data['starting_timestamp']
+			et = form.cleaned_data['ending_timestamp']
+
+			# print(replay_name, starting_timestamp, ending_timestamp)
+	
+			data = models.Replay(
+				replay_name = rn,
+				starting_timestamp = st,
+				ending_timestamp = et,
+			)
+
+			data.save()
+	else:
+		dummy_form_data = {
+			"replay_name": "Replay Name",
+			"starting_timestamp": "Starting Timestamp",
+			"ending_timestamp": "Ending Timestamp",
+		}
+		form = ReplayForm(initial=dummy_form_data)
+
 	return render(
 					request,
 					"home/create_replay_template.html",
 					{
+						"form": form,
 					}
 	)
